6e7b78cdec6029260e7593f07a38f042a8726e96
ysakamoto <yus.sakamoto@gmail.com>
2013-04-15 04:51:38 -0500

    copied from stampede. on stampede, dynamic threading seems working.

--- prob_5/nbody.C
+++ prob_5/nbody.C
@@ -24,17 +24,20 @@ void build_tree( const double xmin,
 				 const int max_pts_per_node,
 				 const int max_level,
 				 point width,
-				 particle* pts,
+				 vector<particle>* pts,
 				 int np,
 				 qtree* qt,
-				 const int st_pt )
+				 const int st_pt,
+				 const int np_proc,
+				 const int nt )
 {
 	point anch(xmin, ymin);
 	
-	qt->initialize_root(NULL, 0, anch, max_level,
-						max_pts_per_node, width, 0, pts, np, st_pt );
+	qt->initialize_root( NULL, 0, anch, max_level,
+						 max_pts_per_node, width, 0,
+						 pts, np, 0, st_pt, np_proc );
 
-	qt->create_kids();
+	qt->insert_points(nt);
 	
 	// for(int i=0; i<4;i++){
 	// 	for(int j=0; j<4; j++){
@@ -49,15 +52,36 @@ void build_tree( const double xmin,
 }
 	
 
+// read from the file
+int read_points( vector<particle>& pts,
+				 const int np )
+{
+	ifstream file_in ("points.dat");
+	if (!file_in.is_open()) return 1;
+
+	pts.resize(np);
+	
+	for(int i=0; i<np; i++){
+		file_in>>pts[i].x;
+		file_in>>pts[i].y;
+		file_in>>pts[i].mt_id;
+	}
+	
+	file_in.close();
+	
+	return 0;
+}
+
+
 int write_points( const vector<particle>& pts,
 				  const int np )
 {
 	ofstream ofile;
 	ofile.open ("points.dat");
 
-	for (int i=0; i<np; i++)
+	for (int i=0; i<np; i++){
 		ofile<<pts[i].x<<" "<<pts[i].y<<" "<<pts[i].mt_id<<endl;
-
+	}
 	ofile.close();
 
 	return 0;
@@ -68,13 +92,28 @@ void average_trees( qtree* qts, const int nt )
 	// for(int i=0; i<nt; i++)
 	// 	qts[i].show_tree();
 
+	// qtree qt;
+	// qt.initialize_root( NULL, 0, anch, max_level,
+	// 					max_pts_per_node, width, 0,
+	// 					pts, np, 0, st_pt, np_proc );
+
+	// for(int i=0; i<nt; i++){
+	// 	for(int j=0; j<4; j++){
+	// 		if
+	// 	}
+
+	// }
 	
-	for(int i=0; i<4; i++){
-		for(int j=0; j<nt; j++){
-			if(qts[j].kids[i].idx.size())
-				cout<<j<<" "<<i<<" "<<qts[j].kids[i].gid<<endl;
-		}
-	}	
+	
+	// for(int i=0; i<4; i++){
+	// 	for(int j=0; j<4; j++){
+	// 		for(int k=0; k<nt; k++){
+	// 			if(qts[k].kids[j].kids[i].idx.size())
+	// 				cout<<k<<" "<<k<<" "<<i<<" "<<
+	// 					qts[k].kids[j].kids[i].gid<<endl;
+	// 		}
+	// 	}
+	// }
 	
 	
 	return;
@@ -82,8 +121,13 @@ void average_trees( qtree* qts, const int nt )
 
 int main()
 {
+	n_th = 2;
+	
+	// thread nesting enabled
+	omp_set_nested(1);
+	
 	// number of threads
-	const int nt = 2;
+	const int nt = 1;
 
 	// domain
 	const double xmin = 0;
@@ -92,16 +136,17 @@ int main()
 	const double ymax = 1;
 
 	// number of poitns
-	const int np = 1000;
+	const int np = 2000000;
 
 	// information necessary for qtree construction
-	const int max_level = 4;
+	const int max_level = 10;
 	const int max_pts_per_node = 1;
 
 	// get domain range and grid size
 	const double xrange = xmax-xmin;
 	const double yrange = ymax-ymin;
 	const double x_grid_size = (xmax-xmin)/pow(2.,max_level);
+	
 	const double y_grid_size = (ymax-ymin)/pow(2.,max_level);
 	const point width(xrange, yrange);
 
@@ -109,9 +154,22 @@ int main()
 	vector<particle> pts;
 	pts.resize(np);
 	#pragma omp parallel for default(none), shared(pts)
-	for(int i=0; i<np; i++)
-		pts[i].gen_coords(xmin, xrange, ymin, yrange);
+	for(int i=0; i<np; i++){
 
+		if(i<np/30)
+			pts[i].gen_coords_cluster(xmin, xrange, ymin, yrange, xmin+xrange/4,
+									  ymin+yrange/3, min(xrange,yrange)/5);
+		else if(i<np/10)
+			pts[i].gen_coords_cluster(xmin, xrange, ymin, yrange, xmin+3*xrange/4,
+									  ymin+2*yrange/3, min(xrange,yrange)/6);
+		else
+		pts[i].gen_coords_cluster(xmin, xrange, ymin, yrange,
+								  xmin+3*xrange/4, ymin,
+								  min(xrange,yrange)/6);
+
+		// pts[i].gen_coords(xmin, xrange, ymin, yrange);
+	}
+	
 	// compute morton ids
 	#pragma omp parallel for default(none), shared(pts)
 	for(int i=0; i<np; i++)
@@ -121,39 +179,65 @@ int main()
 	vector<particle> tmp;
 	tmp.resize(np);
 	mergesort<particle>(&pts[0], nt, np, &tmp[0], compar_vector);
+
+	// read points data
+	read_points(pts, np);
 	
-	// partition ids for parallel tree build
-	int* np_local = new int[nt]; // length of partition
-	int* st_pt = new int[nt]; // starting point of partition
-	for(int i=0; i<nt-1; i++){
-		np_local[i] = np/nt;
-	}
-	np_local[nt-1] = np-np/nt*(nt-1);
-	st_pt[0] = 0;
-	for(int i=1; i<nt; i++){
-		st_pt[i] = st_pt[i-1]+np_local[i-1];
-	}
-
-	// pointer for quadtrees
-	qtree* qts = new qtree[nt];
-
+	qtree* qt1 = new qtree;
 	// parallel tree construction
 	double start=omp_get_wtime();
 	// #pragma omp parallel for shared(pts, np_local)
 	for(int i=0; i<nt; i++){
 		// build tree
 		build_tree( xmin, ymin, max_pts_per_node, max_level, width,
-					&pts[st_pt[i]], np_local[i], &qts[i], st_pt[i]);
+		&pts, np, qt1, 0, np, 1);
 	}
 	double end=omp_get_wtime();
-	
 	cout<<"wall clock time = "<<end-start<<endl;
 
-	average_trees(qts, nt);
+	qtree* qt2 = new qtree;
+	// parallel tree construction
+	start=omp_get_wtime();
+	// #pragma omp parallel for shared(pts, np_local)
+	for(int i=0; i<nt; i++){
+		// build tree
+		build_tree( xmin, ymin, max_pts_per_node, max_level, width,
+		&pts, np, qt2, 0, np, 2);
+	}
+	end=omp_get_wtime();
+	cout<<"wall clock time = "<<end-start<<endl;
+
+	
+	qtree* qt4 = new qtree;
+	// parallel tree construction
+	start=omp_get_wtime();
+	// #pragma omp parallel for shared(pts, np_local)
+	for(int i=0; i<nt; i++){
+		// build tree
+		build_tree( xmin, ymin, max_pts_per_node, max_level, width,
+		&pts, np, qt4, 0, np, 4);
+	}
+	end=omp_get_wtime();
+	cout<<"wall clock time = "<<end-start<<endl;
+
+	qtree* qt8 = new qtree;
+	// parallel tree construction
+	start=omp_get_wtime();
+	// #pragma omp parallel for shared(pts, np_local)
+	for(int i=0; i<nt; i++){
+		// build tree
+		build_tree( xmin, ymin, max_pts_per_node, max_level, width,
+		&pts, np, qt8, 0, np, 8);
+	}
+	end=omp_get_wtime();
+	cout<<"wall clock time = "<<end-start<<endl;
+
+	
+	// average_trees(qts, nt);
 	
 	// write_points(pts, np);
 
-	delete[] qts;
+	delete qt1, qt2, qt4, qt8;
 	
 	return 0;
 }
--- prob_5/qtree.h
+++ prob_5/qtree.h
@@ -28,11 +28,11 @@ class qtree
 public:
 	// using nid and level, we can identify the node
 	int lid; // local node id [0, 1, 2, 3]
-	int gid; // global node id
+	long gid; // global node id
   	int level;   // level of the node 
 	
 	// particles data
-	particle* pts;
+	vector<particle>* pts;
 
 	// particle global ids that a node has
 	vector<int> idx;
@@ -40,7 +40,7 @@ public:
 	// starting point in the array for parallel
 	int st_pt;
 	
-	int np; // number of points
+	int total_np; // number of points
 
 	// pointers to relatives
 	qtree* kids; // 4-size array
@@ -59,57 +59,73 @@ public:
 	// constructor
 	qtree(){;}
 
-	void initialize(qtree* pr, int lv, point anch, int mxlv, int mxpts,
-					point wid, int l_id, particle* pcl, int n, int g_id,
-					int st_point );
-	void initialize_root(qtree* pr, int lv, point anch, int mxlv, int mxpts,
-						 point wid, int id, particle* pcl, int n,
-						 const int st_point )
-	{
-		initialize( pr, lv, anch, mxlv, mxpts,
-					wid, id, pcl, n, 0, st_point );
-		idx.resize(n,0);
-		for(int i=0; i<n; i++){
-			idx[i]=st_point+i;
-		}
-		lid = 0;
-		gid = 0;
-
-	};
-	int insert_points( );
+	void initialize ( qtree* ini_parent, int ini_level, point ini_anchor,
+					  int ini_max_level, int ini_max_pts,
+					  point ini_width, int ini_lid,
+					  vector<particle>* points, int num_points,
+					  long ini_gid, int st_point );
+	void initialize_root( qtree* ini_parent, int ini_level, point ini_anchor,
+						  int ini_max_level, int ini_max_pts,
+						  point ini_width, int ini_lid,
+						  vector<particle>* points, int num_points,
+						  long ini_gid, int st_point,
+						  int np_local);
+	
+	int insert_points( int n_threads=2 );
 	void create_kids();
 	void show_kids();
 	void points_in_node( vector<int>& idx_parent );
 	void show_tree();
 };
 
-void qtree::initialize( qtree* pr, int lv, point anch, int mxlv, int mxpts,
-						point wid, int l_id, particle* pcl, int n,
-						int g_id, int st_point )
+void qtree::initialize_root( qtree* ini_parent, int ini_level, point ini_anchor,
+							 int ini_max_level, int ini_max_pts,
+							 point ini_width, int ini_lid,
+							 vector<particle>* points, int num_points,
+							 long ini_gid, int st_point,
+							 int np_proc )
 {
-	lid = l_id;
-	gid = g_id;
-	level = lv;   // level of the node 
-	pts = pcl;
+	initialize( ini_parent, ini_level, ini_anchor,
+				ini_max_level, ini_max_pts,
+				ini_width, ini_lid, points, num_points, ini_gid, st_point );
+	idx.resize(np_proc,0);
+	for(int i=0; i<np_proc; i++){
+		idx[i]=st_point+i;
+	}
+	lid = 0;
+	gid = 0;
+};
+
+void qtree::initialize ( qtree* ini_parent, int ini_level, point ini_anchor,
+						 int ini_max_level, int ini_max_pts,
+						 point ini_width, int ini_lid,
+						 vector<particle>* points, int num_points,
+						 long ini_gid, int st_point )
+{
+	lid = ini_lid;
+	gid = ini_gid;
+	level = ini_level;   // level of the node 
+	pts = points;
 	// idx
-	np = n;
+	total_np = num_points;
 	// kids
 	// parent
 	isleaf = 1;
-	anchor = anch;  // [x;y] coordinates of lower left point of a node
-	width = wid; // width of a box
-	parent = pr;  // parent of the node
-	max_level = mxlv;
-	max_pts = mxpts;
+	anchor = ini_anchor;  // [x;y] coordinates of lower left point of a node
+	width = ini_width; // width of a box
+	parent = ini_parent;  // parent of the node
+	max_level = ini_max_level;
+	max_pts = ini_max_pts;
 	st_pt = st_point;
+	
 	for(int i=0; i<4; i++)
 		kids_exist[i] = 1;
 }
 
+int n_th;
 
-int qtree::insert_points(  )
+int qtree::insert_points( int n_threads )
 {
-
 	if (isleaf){
 
 		if((level==max_level) || (idx.size()<max_pts)){
@@ -121,6 +137,69 @@ int qtree::insert_points(  )
 
 	}
 
+	if (n_threads > 1){
+		// now insert points to kids
+		// for(int i=0; i<4; i++){
+		#pragma omp parallel sections
+		{
+			#pragma omp section
+			{
+				// cout<<omp_get_thread_num()<<" start"<<endl;
+				kids[0].points_in_node(idx);
+				if(kids[0].insert_points(n_threads/2))
+					kids_exist[0] = 0;
+				
+				kids[1].points_in_node(idx);
+				if(kids[1].insert_points(n_threads/2))
+					kids_exist[1] = 0;
+				// #pragma omp critical
+				// {
+				// 	cout<<omp_get_thread_num()<<" "
+				// 	<<kids[0].idx.size()+kids[1].idx.size()<<" end"<<endl;
+				// }
+			}
+
+			#pragma omp section
+			{
+				// cout<<omp_get_thread_num()<<" start"<<endl;
+				kids[2].points_in_node(idx);
+				if(kids[2].insert_points(n_threads-n_threads/2))
+					kids_exist[2] = 0;
+				
+				kids[3].points_in_node(idx);
+				if(kids[3].insert_points(n_threads-n_threads/2))
+					kids_exist[3] = 0;
+
+				// 	#pragma omp critical
+				// {
+				// 	cout<<omp_get_thread_num()<<" "
+				// 	<<kids[2].idx.size()+kids[3].idx.size()<<" end"<<endl;
+				// }
+				
+
+			}
+
+		} 
+	}
+
+	// serial
+	else{
+		for(int i=0; i<4; i++){
+			kids[i].points_in_node(idx);
+			if(kids[i].insert_points(1))
+				kids_exist[i] = 0;
+		}
+			
+	}
+	// cout<<"RESTL "<<kids[i].lid<<endl;
+	// for(int j=0; j<kids[i].idx.size(); j++)
+	// cout<<kids[i].idx[j]<<endl;
+		
+	
+	
+	//} end for
+
+	
 	return 0;
 
 }
@@ -131,36 +210,24 @@ void qtree::create_kids()
 	point kid_width;
 	kid_width.x = width.x/2.0;
 	kid_width.y = width.y/2.0;
-
 	point kid_anchor;
 
+	// create new kids objects
 	kids  = new qtree[4];
 	
 	for(int i=0; i<4; i++){
 		kid_anchor.x = anchor.x + kid_width.x*kids_pos[i][0];
 		kid_anchor.y = anchor.y + kid_width.y*kids_pos[i][1];
 
+		// kid_global_id = 4*parent_global_id + kid_local_id
 		kids[i].initialize(this, kid_level, kid_anchor,
-						   max_level, max_pts, kid_width, i, pts, np,
+						   max_level, max_pts, kid_width, i, pts, total_np,
 						   i+gid*4, st_pt );
 	}
 
-	// not leaf any more
+	// not a leaf any more
 	isleaf = 0;
-	
-	// now insert points to kids
-	for(int i=0; i<4; i++){
-		
-		kids[i].points_in_node(idx);
-
-		// cout<<"RESTL "<<kids[i].lid<<endl;
-		// for(int j=0; j<kids[i].idx.size(); j++)
-			// cout<<kids[i].idx[j]<<endl;
-		
-		if(kids[i].insert_points())
-			kids_exist[i] = 0;
-	}
-		
+			
 	// show_kids();
 	
 	return;
@@ -174,19 +241,19 @@ void qtree::show_kids()
 
 }
 
+// check which pointes are in nodes
 void qtree::points_in_node(vector<int>& idx_parent)
 {
+	// number of points that a parent has 
 	int npts = idx_parent.size();
-
 		
 	for(int i=0; i<npts; i++){
-		int real_idx = idx_parent[i]-st_pt;
-
-		int mt_id = (pts)[real_idx].mt_id;
+		// int mt_id = (*pts)[real_idx].mt_id;
+		int mt_id = (*pts)[idx_parent[i]].mt_id;
 		int offset = max_level-level;
 	
 		if(( (mt_id & mt_checker[offset]) >> (offset*2)  ) == lid){
-			idx.push_back(real_idx+st_pt);
+			idx.push_back(idx_parent[i]);
 			// cout<<real_idx<<" "<<(pts)[real_idx].x<<" "<<(pts)[real_idx].y
 				// <<" "<<(pts)[real_idx].mt_id<<" is in box "<<lid
 				// <<" on level "<<level<<endl;
@@ -208,7 +275,8 @@ void qtree::show_tree()
 			kids[i].show_tree();
 		}
 		else{
-			cout<<kids[i].gid<<" leaf"<<endl;
+			cout<<"level: "<<kids[i].level
+				<<" id: "<<kids[i].gid<<endl;
 			for(int j=0; j<kids[i].idx.size(); j++)
 				cout<<kids[i].idx[j]<<endl;
 		}
